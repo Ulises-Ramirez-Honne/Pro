fc27c76801aaf374e306d3ad175061820640f456
Ulises-Ramirez-Honne <ulises.ramirez@honneservices.com>
2025-05-19 14:53:34 -0600

    Update Script.py
    Ulises Nova pro Change

--- Script.py
+++ Script.py
@@ -87,7 +87,8 @@ def clasificacion_documentos(bucket, key):
     inf_config = {"maxTokens": 5000, "temperature": 0.1}
 
     response = bedrock.converse(
-        modelId="us.amazon.nova-lite-v1:0",
+        # modelId="us.amazon.nova-lite-v1:0",
+        modelId="amazon.nova-pro-v1:0",
         messages=messages,
         inferenceConfig=inf_config
     )
